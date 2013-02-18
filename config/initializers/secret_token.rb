40f04276ca38cd2e1dbbb97a86391760525a5409
zaracato <zaracato@gmail.com>
2013-02-18 05:05:43 -0600

    soy zaracato3

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FullcalendarRails::Application.config.secret_token = 'b5ad1ae881216a01984ad77d52685894443ab16071b1f52bb2bb91341ef74ef8ad7fa3491c5a539e096d4d460a4606633744611b9c812468c6939ec46fa51b94'
